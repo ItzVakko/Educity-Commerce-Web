b97655f4f924cd4367095b691e6f5981a8fbbdc6
ItzVakko <itzvakkoofficial@gmail.com>
2024-08-10 23:49:47 +0400

    edited checkout card system

--- src/app/Components/GlobalComponents/CartCard.jsx
+++ src/app/Components/GlobalComponents/CartCard.jsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 import Image from "next/image";
-import MenImg from "../../Assets/Images/MensClothes/beautiful-male-model-holding-hand-hair 1.png";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { useDispatch, useSelector } from "react-redux";
 import { delItem } from "@/app/Store/Reducers/cartReducer";
@@ -8,39 +7,58 @@ import { delItem } from "@/app/Store/Reducers/cartReducer";
 import "./CartCard.css";
 
 const CartCard = ({ item }) => {
-  const formattedPrice = useMemo(
-    () => `${item.price}${item.currency}`,
-    [item.currency, item.price]
-  );
+  const dispatch = useDispatch();
+
+  const colorImage =
+    item.colorImages.find((colorImg) => colorImg.color === item.selectedColor)
+      ?.image || item.mainImage.image;
 
   const qty = useSelector(
     (state) =>
-      state.cart.items.find((cartItem) => cartItem._id === item._id)?.qty || 0
+      state.cart.items.find(
+        (cartItem) =>
+          cartItem._id === item._id &&
+          cartItem.selectedSize === item.selectedSize &&
+          cartItem.selectedColor === item.selectedColor
+      )?.qty || 0
   );
 
-  const dispatch = useDispatch();
+  const handleRemove = useCallback(() => {
+    dispatch(
+      delItem({
+        _id: item._id,
+        selectedSize: item.selectedSize,
+        selectedColor: item.selectedColor,
+      })
+    );
+  }, [dispatch, item._id, item.selectedSize, item.selectedColor]);
+
+  const formattedPrice = `${item.price} ${item.currency}`;
 
   return (
     <div className="cart-card-wrapper">
-      <button
-        className="delete-item-button"
-        onClick={() => dispatch(delItem({ _id: item._id }))}
-      >
+      <button className="delete-item-button" onClick={() => handleRemove()}>
         <CloseRoundedIcon />
       </button>
 
-      <Image src={MenImg} alt="product-image" className="cart-card-img" />
+      <Image
+        src={colorImage}
+        width={100}
+        height={100}
+        alt="product-image"
+        className="cart-card-img"
+      />
 
       <div className="cart-card-details-wrapper">
         <h4>{item.model}</h4>
 
         <p className="product-properties">
-          ზომა: <span>{item.size}</span>
+          ზომა: <span>{item.selectedSize}</span>
         </p>
 
         <div className="price-and-color-wrapper">
           <p className="product-properties">
-            ფერი: <span> შავი</span>
+            ფერი: <span>{item.selectedColor}</span>
           </p>
 
           <span className="product-price">
